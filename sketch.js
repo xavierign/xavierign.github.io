2a92c79423b195b72361b914de2ac5dbef9c3b12
xavierign <xig2000@columbia.edu>
2021-12-06 16:25:14 -0300

    editing scketch

--- sketch.js
+++ sketch.js
@@ -1,11 +1,16 @@
 function setup() {
   createCanvas(400, 400);
   background(220);
+  fill(255)
+  ellipse(224, 184, 220, 220);
+  i = 0
 }
 
 function draw() {
-  fill(255);
-  rect(100,100,100,100);
-  
-  
+  //background(220);
+  fill(0,i,100)
+  ellipse(224+i, 184-i, 20, 20);
+  i =  i +1;
+  fill(255,0,0);
+  rect(100,100,40+i);
 }
